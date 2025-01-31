47e73fb64ed06c371f3d94983c52545aba377c69
Kimminsub1 <awdx303@gmail.com>
2025-01-31 16:00:51 +0900

    mm

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import MainPage from './components/MainPage.jsx'; // 확장자 변경
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
