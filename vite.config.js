accb579320bbe7459cd36b14030ca06e60aa0d99
KMS <awdx303@gmail.com>
2025-02-10 20:34:49 +0900

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MinSub-Portpolio/", // 여기에 GitHub Pages 리포지토리 이름 입력!
+  base: "/MinSub-Portpolio/", 
 });
