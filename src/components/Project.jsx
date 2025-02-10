6ba0fe1491784cd5f76b431d15fbbafc7b19d6f4
KMS <awdx303@gmail.com>
2025-02-11 01:19:45 +0900

    aa

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -28,28 +28,28 @@ const Projects = () => {
       <div className="projects-content">
         {activeTab === "Project1" && (
           <div className="project-item">
-            <img src="/img/project_usinsa.png" alt="project_usinsa" />
+            <img src="img/project_usinsa.png" alt="project_usinsa" />
             <h3>남성의류 쇼핑몰 웹사이트</h3>
             <p>JAVA와 MySQL을 이용하여 개발한 쇼핑몰 프로젝트</p>
           </div>
         )}
         {activeTab === "Project2" && (
           <div className="project-item">
-            <img src="/img/project_kiosk.png" alt="project_kiosk" />
+            <img src="img/project_kiosk.png" alt="project_kiosk" />
             <h3>스마트폰 키오스크</h3>
             <p>JAVA와 MySQL을 이용하여 개발한 키오스크 프로젝트</p>
           </div>
         )}
         {activeTab === "Project3" && (
           <div className="project-item">
-            <img src="/img/project_sagwasangjum.png" alt="project_sagwasangjum" />
+            <img src="img/project_sagwasangjum.png" alt="project_sagwasangjum" />
             <h3>전자자제품 쇼핑몰</h3>
             <p>Servlet과 Apache를 사용하여 제작한 웹사이트</p>
           </div>
         )}
         {activeTab === "Project4" && (
           <div className="project-item">
-            <img src="/img/project3.jpg" alt="프로젝트 4" />
+            <img src="img/project3.jpg" alt="프로젝트 4" />
             <h3>React & SpringBoot 프로젝트</h3>
             <p>React와 Spring Boot를 활용한 풀스택 웹 애플리케이션</p>
           </div>
