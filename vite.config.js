7b30d37408c6163984a9aea30d87516aaa72014c
KMS <awdx303@gmail.com>
2025-02-10 20:30:03 +0900

    cwee

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/MinSub-Portpolio/", // 여기에 GitHub Pages 리포지토리 이름 입력!
+});
