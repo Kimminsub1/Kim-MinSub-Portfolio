cb9749535c5539c24ca5c86f45ca89514fed4ff7
KMS <awdx303@gmail.com>
2025-02-11 01:15:41 +0900

    project_update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,12 +11,15 @@ const Projects = () => {
       {/* 탭 메뉴 */}
       <div className="projects-sidebar">
         <button className={activeTab === "Project1" ? "active" : ""} onClick={() => setActiveTab("Project1")}>
-          프로젝트 1
+        Usinsa
         </button>
         <button className={activeTab === "Project2" ? "active" : ""} onClick={() => setActiveTab("Project2")}>
-          프로젝트 2
+        SmartPhone Kiosk
         </button>
         <button className={activeTab === "Project3" ? "active" : ""} onClick={() => setActiveTab("Project3")}>
+        Sagwasangjum
+        </button>
+        <button className={activeTab === "Project4" ? "active" : ""} onClick={() => setActiveTab("Project4")}>
           프로젝트 3
         </button>
       </div>
@@ -25,21 +28,28 @@ const Projects = () => {
       <div className="projects-content">
         {activeTab === "Project1" && (
           <div className="project-item">
-            <img src="/img/project1.jpg" alt="프로젝트 1" />
-            <h3>의류 쇼핑몰 웹사이트</h3>
+            <img src="/img/project_usinsa.png" alt="프로젝트 1" />
+            <h3>남성의류 쇼핑몰 웹사이트</h3>
             <p>JAVA와 MySQL을 이용하여 개발한 쇼핑몰 프로젝트</p>
           </div>
         )}
         {activeTab === "Project2" && (
           <div className="project-item">
-            <img src="/img/project2.jpg" alt="프로젝트 2" />
-            <h3>전자제품 쇼핑몰</h3>
-            <p>Servlet과 Apache를 사용하여 제작한 웹사이트</p>
+            <img src="/img/project_kiosk.png" alt="프로젝트 2" />
+            <h3>스마트폰 키오스크</h3>
+            <p>JAVA와 MySQL을 이용하여 개발한 키오스크 프로젝트</p>
           </div>
         )}
         {activeTab === "Project3" && (
           <div className="project-item">
-            <img src="/img/project3.jpg" alt="프로젝트 3" />
+            <img src="/img/project_sagwasangjum.png" alt="프로젝트 3" />
+            <h3>전자자제품 쇼핑몰</h3>
+            <p>Servlet과 Apache를 사용하여 제작한 웹사이트</p>
+          </div>
+        )}
+        {activeTab === "Project4" && (
+          <div className="project-item">
+            <img src="/img/project3.jpg" alt="프로젝트 4" />
             <h3>React & SpringBoot 프로젝트</h3>
             <p>React와 Spring Boot를 활용한 풀스택 웹 애플리케이션</p>
           </div>
