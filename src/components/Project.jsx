887156d4eb20276d46ae382f67ac9fafef80b3f5
KMS <awdx303@gmail.com>
2025-02-11 19:40:26 +0900

    aa

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,45 @@ import "./Project.css";
 const Projects = () => {
   // 선택된 탭을 추적하는 상태
   const [activeTab, setActiveTab] = useState("Project1");
+  const [imageIndex, setImageIndex] = useState(0); // 현재 슬라이드 인덱스
+
+  // 프로젝트별 이미지 배열
+  const projectImages = {
+    Project1: [
+      "/MinSub-Portpolio/img/project_usinsa1.png",
+      "/MinSub-Portpolio/img/project_usinsa2.png",
+      "/MinSub-Portpolio/img/project_usinsa3.png",
+    ],
+    Project2: [
+      "/MinSub-Portpolio/img/project_kiosk1.png",
+      "/MinSub-Portpolio/img/project_kiosk2.png",
+    ],
+    Project3: [
+      "/MinSub-Portpolio/img/project_sagwasangjum1.png",
+      "/MinSub-Portpolio/img/project_sagwasangjum2.png",
+    ],
+    Project4: [
+      "/MinSub-Portpolio/img/kokeetea1.png",
+      "/MinSub-Portpolio/img/kokeetea2.png",
+    ],
+  };
+
+  // 현재 선택된 프로젝트의 이미지 배열 가져오기
+  const images = projectImages[activeTab];
+
+  // 이전 이미지 보기
+  const prevImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  // 다음 이미지 보기
+  const nextImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
   return (
     <div className="projects-container">
@@ -12,25 +51,37 @@ const Projects = () => {
       <div className="projects-sidebar">
         <button
           className={activeTab === "Project1" ? "active" : ""}
-          onClick={() => setActiveTab("Project1")}
+          onClick={() => {
+            setActiveTab("Project1");
+            setImageIndex(0);
+          }}
         >
           Usinsa
         </button>
         <button
           className={activeTab === "Project2" ? "active" : ""}
-          onClick={() => setActiveTab("Project2")}
+          onClick={() => {
+            setActiveTab("Project2");
+            setImageIndex(0);
+          }}
         >
           SmartPhone Kiosk
         </button>
         <button
           className={activeTab === "Project3" ? "active" : ""}
-          onClick={() => setActiveTab("Project3")}
+          onClick={() => {
+            setActiveTab("Project3");
+            setImageIndex(0);
+          }}
         >
           Sagwasangjum
         </button>
         <button
           className={activeTab === "Project4" ? "active" : ""}
-          onClick={() => setActiveTab("Project4")}
+          onClick={() => {
+            setActiveTab("Project4");
+            setImageIndex(0);
+          }}
         >
           KokeeTea
         </button>
@@ -38,35 +89,35 @@ const Projects = () => {
 
       {/* 프로젝트 내용 */}
       <div className="projects-content">
-        {activeTab === "Project1" && (
+        {images && (
           <div className="project-item">
-            <img src="img/project_usinsa.png" alt="project_usinsa" />
-            <h3>남성의류 쇼핑몰 웹사이트</h3>
-            <p>JAVA와 MySQL을 이용하여 개발한 쇼핑몰 프로젝트</p>
-          </div>
-        )}
-        {activeTab === "Project2" && (
-          <div className="project-item">
-            <img src="img/project_kiosk.png" alt="project_kiosk" />
-            <h3>스마트폰 키오스크</h3>
-            <p>JAVA와 MySQL을 이용하여 개발한 키오스크 프로젝트</p>
-          </div>
-        )}
-        {activeTab === "Project3" && (
-          <div className="project-item">
-            <img
-              src="img/project_sagwasangjum.png"
-              alt="project_sagwasangjum"
-            />
-            <h3>전자자제품 쇼핑몰</h3>
-            <p>Servlet과 Apache를 사용하여 제작한 웹사이트</p>
-          </div>
-        )}
-        {activeTab === "Project4" && (
-          <div className="project-item">
-            <img src="img/kokeetea.png" alt="프로젝트 4" />
-            <h3>Cafe_KokeeTea</h3>
-            <p>React와 Spring Boot를 활용한 풀스택 웹 애플리케이션</p>
+            <div className="image-slider">
+              <button className="prev-btn" onClick={prevImage}>
+                {"<"}
+              </button>
+              <img src={images[imageIndex]} alt={`slide-${imageIndex}`} />
+              <button className="next-btn" onClick={nextImage}>
+                {">"}
+              </button>
+            </div>
+            <h3>
+              {activeTab === "Project1"
+                ? "남성의류 쇼핑몰 웹사이트"
+                : activeTab === "Project2"
+                ? "스마트폰 키오스크"
+                : activeTab === "Project3"
+                ? "전자제품 쇼핑몰"
+                : "Cafe_KokeeTea"}
+            </h3>
+            <p>
+              {activeTab === "Project1"
+                ? "JAVA와 MySQL을 이용하여 개발한 쇼핑몰 프로젝트"
+                : activeTab === "Project2"
+                ? "JAVA와 MySQL을 이용하여 개발한 키오스크 프로젝트"
+                : activeTab === "Project3"
+                ? "Servlet과 Apache를 사용하여 제작한 웹사이트"
+                : "React와 Spring Boot를 활용한 풀스택 웹 애플리케이션"}
+            </p>
           </div>
         )}
       </div>
