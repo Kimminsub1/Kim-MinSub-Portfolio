ba8eecdbba51c6127ae160979023d7bb0b07ea07
KMS <awdx303@gmail.com>
2025-02-12 07:02:21 +0900

    aa

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,11 +4,16 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <section className="kms-section">
+      
+      <div className="kms-image">
+        <img src="img/kms.jpg" alt="프로필 이미지" />
+      </div>
+
       <div className="kms-content">
         <h1>안녕하세요.</h1>
         <h2>주니어 백엔드 개발자 김민섭입니다.</h2>
-        <h2>학력</h2>
-        
+        <h2>학력 : 남서울대학교 &lt;컴퓨터학과&gt;</h2>
+
         <h2>Contact me</h2>
         <ul>
           <li>
@@ -25,9 +30,7 @@ const HeroSection = () => {
           </li>
         </ul>
       </div>
-      <div className="kms-image">
-        <img src="img/kms.jpg" alt="프로필 이미지" />
-      </div>
+      
     </section>
   );
 };
